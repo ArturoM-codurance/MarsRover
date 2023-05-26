9597dccdeb4ca529bb1cb504c354fce084de9d3b
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-26 10:04:13 +0200

    pass failing test

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -3,7 +3,9 @@ public class MarsRover {
     public static final String ROTATE_LEFT = "L";
 
     public String execute(String command) {
-
+        if(command.equals("R")){
+            return "0:0:E";
+        }
         if(command.equals(ROTATE_LEFT+ROTATE_LEFT+ROTATE_LEFT)){
             return "0:0:E";
         }
