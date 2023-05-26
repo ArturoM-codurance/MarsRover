e05faa3744717aa74eff3072960b3219cbc8503e
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-26 12:18:24 +0200

    add failing test

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -50,6 +50,7 @@ public class MarsRoverShould {
             "MM, 0:2:N",
             "MMM, 0:3:N",
             "MMMRRM, 0:2:S",
+            "MMMMMMMMMM, 0:0:N"
     })
     void move_YAxis(String commands, String expectedPosition){
         MarsRover marsRover = new MarsRover();
