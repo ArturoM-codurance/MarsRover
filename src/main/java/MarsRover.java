21b067d3c793aa9d9ddb7018c064c3414fddd4fc
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-26 09:43:56 +0200

    pass faiing test

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,5 +1,8 @@
 public class MarsRover {
     public String execute(String command) {
+        if(command.equals("L")){
+            return "0:0:W";
+        }
         return "0:0:N";
     }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -15,4 +15,13 @@ public class MarsRoverShould {
         String expectedPosition = "0:0:N";
         assertEquals(expectedPosition, currentPosition);
     }
+    @Test
+    void be_at_position_0_0_W_when_single_L_received(){
+        MarsRover marsRover = new MarsRover();
+
+        String currentPosition = marsRover.execute("L");
+
+        String expectedPosition = "0:0:W";
+        assertEquals(expectedPosition, currentPosition);
+    }
 }
