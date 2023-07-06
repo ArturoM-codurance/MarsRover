04b1d7bbe67cc26cf10e74ca7e36191022908e07
Arturo Menendez <arturo.menendez@codurance.com>
2023-07-06 13:01:33 +0200

    cors policy

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,6 +1,11 @@
 resource "aws_apigatewayv2_api" "lambda" {
   name          = "serverless_lambda_gw"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["*"]
+    allow_headers = ["*"]
+  }
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
