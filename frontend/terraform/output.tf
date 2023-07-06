addc41a5b3cb3dce665f78e2036f377fc957f582
Arturo Menendez <arturo.menendez@codurance.com>
2023-07-06 21:44:20 +0200

    terraform output

--- frontend/terraform/output.tf
+++ frontend/terraform/output.tf
@@ -1,3 +1,7 @@
 output "frontend_endpoint" {
   value = aws_s3_bucket_website_configuration.frontend_example.website_endpoint
 }
+
+output "invocation_url" {
+  value = aws_s3_bucket_website_configuration.frontend_example.website_endpoint
+}
