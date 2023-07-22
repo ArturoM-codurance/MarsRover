4b2b4d705467f19e5c52ad11701f0b4c6b63ce01
Arturo Menendez <arturo.menendez@codurance.com>
2023-07-22 12:01:18 +0200

    finished rotate

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MarsRover {
-    public String execute(String command) {
-        return "0:0:N";
+
+    private String direction = "N";
+    public String execute(String commands) {
+        String coordinates = "0:0:";
+
+        for (char command : commands.toCharArray()) {
+
+            if (command == 'L') {
+                ArrayList directionsCounterClockwise = new ArrayList<>(List.of("N", "W", "S", "E"));
+                direction = rotate(directionsCounterClockwise);
+            }
+
+            if (command == 'R') {
+                ArrayList directionsClockwise = new ArrayList<>(List.of("N", "E", "S", "W"));
+                direction = rotate(directionsClockwise);
+            }
+
+        }
+
+        return coordinates + direction;
+    }
+
+    private String rotate(ArrayList directions) {
+        int currentDirectionIndex = directions.indexOf(direction);
+        direction = directions.get((currentDirectionIndex + 1) % directions.toArray().length).toString();
+        return direction;
     }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.concurrent.SubmissionPublisher;
 
@@ -6,13 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverShould {
 
-    @Test
-    void be_at_position_0_0_N_when_no_commands_received(){
+    @ParameterizedTest
+    @CsvSource({
+            " '', '0:0:N'",
+            " 'R', '0:0:E'",
+            " 'RR', '0:0:S'",
+            " 'RRR', '0:0:W'",
+            " 'RRRR', '0:0:N'",
+            " 'RRRRR', '0:0:E'",
+            " 'L', '0:0:W'",
+            " 'LL', '0:0:S'",
+            " 'LLL', '0:0:E'",
+            " 'LLLL', '0:0:N'",
+            " 'LLLLL', '0:0:W'",
+            " 'LRR', '0:0:E'",
+    })
+    void be_at_position_0_0_N_when_no_commands_received(String input, String output) {
         MarsRover marsRover = new MarsRover();
 
-        String currentPosition = marsRover.execute("");
+        String currentPosition = marsRover.execute(input);
 
-        String expectedPosition = "0:0:N";
-        assertEquals(expectedPosition, currentPosition);
+        assertEquals(output, currentPosition);
     }
 }
